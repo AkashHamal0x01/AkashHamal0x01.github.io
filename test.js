9b6a7f31ef410099ea393dc38b2fed8a9e3d1c7d
Akash Hamal <103249298+AkashHamal0x01@users.noreply.github.com>
2024-09-21 03:36:20 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
-hahha
+var googleApiKey = "AIzaSyD4u4F-5Lw_XeSeM_DVXjNz-NBl_1iVf3M";
+var awsAccessKey = "AKIAIOSFODNN7EXAMPLE";
+var awsSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+var stripeLiveKey = "sk_live_4eC39HqLyjWDarjtT1zdp7dc";
+var mailgunApiKey = "key-3ax6xnjp29jd6fds4gc373sgvjxteol0";
+
+var adminEmail = "admin@example.com";
+var supportEmail = "support@example.org";
+
+var internalIP = "192.168.1.1";
+var externalIP = "203.0.113.5";
+
+eval("alert('This is unsafe!')");
+document.write("Hello, World!");
+
+var apiEndpoint = fetch("https://example.com/api/v1/users");
+var xhrRequest = new XMLHttpRequest();
+xhrRequest.open("GET", "https://example.com/hidden/endpoint", true);
+xhrRequest.send();
+
+var dbPassword = "P@ssw0rd123";
+var dbUsername = "dbUser";
+var ftpPassword = "ftpPassword1!";
+var jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+o
+console.log("Debugging output");
+
